a250fb7723e2820cf69536216e915da7ba10e47c
kimti <kimtyshrestha@gmail.com>
2024-04-17 14:53:49 +0545

    tasks

---
+++ week1/Variable.java
@@ -0,0 +1,28 @@
+package week1;
+
+public class Variable {
+    public class VariableClass {
+        public static void main(String[] args) {
+            /* Non primitive data type 
+             * decalaration          */
+            byte byteValue;
+            /* Initilizarion */
+            byteValue = 10;
+            /* decalaration and initilization  */
+            short shortValue = 100 ;
+            /* multiple declaration */    
+            int intValue1, intValue2;
+            /* multiple unitilization */
+            intValue1 = -99; intValue2 = 99;
+            /*multiple declaration and initilization          */
+            long longValue = -999, longvalue2 = 999;
+            float floatValue = 1.10f;  // the leytter 'f' id  required 
+        
+            double doubleValue = 28.88; // the letter 'd' is optional
+            char charValue ='C' ; // single letter in single quotes
+    
+            boolean boolValue = true; // true or false lowercase 
+     
+    }
+    
+}
